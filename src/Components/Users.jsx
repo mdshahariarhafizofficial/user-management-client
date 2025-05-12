42c9c3f0fb599acfa6f5f3e76db4b6db72987772
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-12 20:40:20 +0600

    send new user  data in server

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -2,10 +2,36 @@ import React, { use } from 'react';
 
 const Users = ({usersPromise}) => {
     const users = use(usersPromise);
-    console.log(users);
+    const handleAddData = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const user = {name, email};
+
+        // Post Data
+        fetch('http://localhost:3000/users',
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type" : "application/json",
+                },
+                body: JSON.stringify(user)
+            }
+        )
+        .then(res => res.json())
+        .then(data => console.log("After Post Data = ", data)
+        )
+        
+    };
     
     return (
         <div>
+            <form onSubmit={handleAddData}>
+                <input type="text" name='name' placeholder='Name' /><br /><br />
+                <input type="email" name='email' placeholder='Email' /><br /><br />
+                <input type="submit" value="Submit" />
+            </form>
+
             {
                 users.map(user => 
                     <p key={user.id}>{user.name}, {user.email}</p>
